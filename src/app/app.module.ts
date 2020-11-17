5068201c014ebd76a83f8a33fb0cac09b9c8ad42
Hubert <hwawrzacz.dev@gmail.com>
2020-11-17 19:52:05 +0100

    Add pages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
+import { TaskDetailsComponent } from './pages/task-details/task-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    TasksComponent,
+    StatisticsComponent,
+    ProjectDetailsComponent,
+    TaskDetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
