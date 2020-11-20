733fd01f9017a4e5c6d5e4c01bbd2fc08fa32324
Hubert <hwawrzacz.dev@gmail.com>
2020-11-20 14:39:06 +0100

    Extend common table component via project list

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,119 +1,84 @@
 import { Component } from '@angular/core';
-import { Project } from 'src/app/model/project';
-
-export interface ColumnDefinition {
-  defName: string;
-  displayName: string;
-  propertyName: string;
-}
-
-export interface ActionDefinition {
-  icon: string;
-  action: (id: number) => void;
-  color?: string;
-  tooltip?: string;
-}
+import { CommonTableComponent } from 'src/app/components/common-table/common-table.component';
 
 @Component({
   selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  templateUrl: '../../components/common-table/common-table.component.html',
+  styleUrls: ['../../components/common-table/common-table.component.scss']
 })
-export class ProjectsComponent {
-  private _pageTitle: string;
-  private _columnsDefinitions: ColumnDefinition[] = [
-    {
-      defName: 'id',
-      displayName: 'ID',
-      propertyName: 'stringId',
-    },
-    {
-      defName: 'title',
-      displayName: 'Title',
-      propertyName: 'title',
-    },
-    {
-      defName: 'manager',
-      displayName: 'Manager',
-      propertyName: 'manager',
-    },
-    {
-      defName: 'employees',
-      displayName: 'Employees',
-      propertyName: 'workersCount',
-    },
-  ];
-
-  private _actionsDefinitions: ActionDefinition[] = [
-    {
-      icon: 'pie_chart',
-      action: (id: number) => {
-        console.log(`statistics for ${id}`);
-      },
-      color: 'primary',
-      tooltip: 'Show statistics'
-    },
-    {
-      icon: 'edit',
-      action: (id: number) => {
-        console.log(`edit ${id}`);
-      },
-      tooltip: 'Edit item'
-    },
-    {
-      icon: 'delete',
-      action: (id: number) => {
-        console.log(`delete ${id}`);
-      },
-      color: 'warn',
-      tooltip: 'Delete item'
-    }
-  ]
-
-  private _dataSource: Project[] = [
-    {
-      id: 1,
-      stringId: 'PROJ_1',
-      title: 'Jeden projekt',
-      description: 'Taki niezwykły projekt, że to to prostu szok i niedowierzanie',
-      manager: 'John Wick',
-      workersCount: 4,
-    },
-    {
-      id: 2,
-      stringId: 'PROJ_2',
-      title: 'Dwa projekt',
-      description: 'Taki drugi projekt z trochę mniejszym szokiem, ale jednak nadal trochę',
-      manager: 'Steven Spielberg',
-      workersCount: 7,
-    }
-  ]
-
+export class ProjectsComponent extends CommonTableComponent {
   constructor() {
-    this._pageTitle = 'Projects';
-  }
+    super();
 
-  get pageTitle(): string {
-    return this._pageTitle;
-  }
+    this._pageTitle = "Projects";
+    this._themeItemNameSingle = "project";
 
-  get dataSource(): Project[] {
-    return this._dataSource;
-  }
+    this._columnsDefinitions = [
+      {
+        defName: 'id',
+        displayName: 'ID',
+        propertyName: 'stringId',
+      },
+      {
+        defName: 'title',
+        displayName: 'Title',
+        propertyName: 'title',
+      },
+      {
+        defName: 'manager',
+        displayName: 'Manager',
+        propertyName: 'manager',
+      },
+      {
+        defName: 'employees',
+        displayName: 'Employees',
+        propertyName: 'workersCount',
+      },
+    ];
 
-  get columnsToDisplay(): string[] {
-    return this._columnsDefinitions.map(item => item.defName).concat(this.actionsDefined ? 'actions' : null).filter(col => !!col);
-  }
+    this._actionsDefinitions = [
+      {
+        icon: 'pie_chart',
+        action: (id: number) => {
+          console.log(`statistics for ${id}`);
+        },
+        color: 'primary',
+        tooltip: 'Show statistics'
+      },
+      {
+        icon: 'edit',
+        action: (id: number) => {
+          console.log(`edit ${id}`);
+        },
+        tooltip: 'Edit item'
+      },
+      {
+        icon: 'delete',
+        action: (id: number) => {
+          console.log(`delete ${id}`);
+        },
+        color: 'warn',
+        tooltip: 'Delete item'
+      }
+    ]
 
-  get columnsDefinitions(): ColumnDefinition[] {
-    return this._columnsDefinitions;
-  }
-
-  get actionsDefinitions(): ActionDefinition[] {
-    return this._actionsDefinitions;
-  }
-
-  get actionsDefined(): boolean {
-    return this._actionsDefinitions.length > 0;
+    this._dataSource = [
+      {
+        id: 1,
+        stringId: 'PROJ_1',
+        title: 'Jeden projekt',
+        description: 'Taki niezwykły projekt, że to to prostu szok i niedowierzanie',
+        manager: 'John Wick',
+        workersCount: 4,
+      },
+      {
+        id: 2,
+        stringId: 'PROJ_2',
+        title: 'Dwa projekt',
+        description: 'Taki drugi projekt z trochę mniejszym szokiem, ale jednak nadal trochę',
+        manager: 'Steven Spielberg',
+        workersCount: 7,
+      }
+    ]
   }
 }
