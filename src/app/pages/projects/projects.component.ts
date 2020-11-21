8efbe161b2bd85b5f6e14a59f0752d8b5463e233
Hubert <hwawrzacz.dev@gmail.com>
2020-11-21 10:56:55 +0100

    Add getting initial data mock
    - move getting data functionality to common table component,
    - add CommonDataSource class,
    -
    - optimize imports.

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { CommonTableComponent } from 'src/app/components/common-table/common-table.component';
+import { Project } from 'src/app/model/project';
+import { ProjectsListService } from 'src/app/services/projects-list.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: '../../components/common-table/common-table.component.html',
   styleUrls: ['../../components/common-table/common-table.component.scss']
 })
-export class ProjectsComponent extends CommonTableComponent {
-  constructor() {
+export class ProjectsComponent extends CommonTableComponent<Project> implements OnInit {
+  constructor(http: HttpClient) {
     super();
 
     this._pageTitle = "Projects";
     this._themeItemNameSingle = "project";
-
     this._columnsDefinitions = [
       {
         defName: 'id',
@@ -62,23 +64,10 @@ export class ProjectsComponent extends CommonTableComponent {
       }
     ]
 
-    this._dataSource = [
-      {
-        id: 1,
-        stringId: 'PROJ_1',
-        title: 'Jeden projekt',
-        description: 'Taki niezwykły projekt, że to to prostu szok i niedowierzanie',
-        manager: 'John Wick',
-        workersCount: 4,
-      },
-      {
-        id: 2,
-        stringId: 'PROJ_2',
-        title: 'Dwa projekt',
-        description: 'Taki drugi projekt z trochę mniejszym szokiem, ale jednak nadal trochę',
-        manager: 'Steven Spielberg',
-        workersCount: 7,
-      }
-    ]
+    this.restService = new ProjectsListService(http);
+  }
+
+  public ngOnInit(): void {
+    super.ngOnInit();
   }
 }
--- src/app/services/projects-list.service.ts
+++ src/app/services/projects-list.service.ts
@@ -1,22 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { stringify } from 'querystring';
-import { Observable } from 'rxjs';
-import { Filter } from '../model/filter';
-import { Pagination } from '../model/pagination';
 import { Project } from '../model/project';
 import { CommonRestService } from './common-rest.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProjectsListService extends CommonRestService<Project> {
-
+export class ProjectsListService extends CommonRestService<Project[]> {
   constructor(protected http: HttpClient) {
     super(http);
-  }
-
-  public getProjects(query: string, pagination?: Pagination, filters?: Filter[]): Observable<Project> {
-    return this.get(query, pagination, filters);
+    this.url = 'http://someurladdress.com/projects'
   }
 }
