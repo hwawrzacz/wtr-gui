2b695ae8d2c9a63309f0db198a67762ecb56dae9
Hubert <hwawrzacz.dev@gmail.com>
2020-11-27 14:26:59 +0100

    Add logic to project tasks component

--- src/app/components/project-tasks/project-tasks.component.ts
+++ src/app/components/project-tasks/project-tasks.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { Filter } from 'src/app/model/filter';
+import { Query } from 'src/app/model/query';
 import { Task } from 'src/app/model/task';
+import { TasksListService } from 'src/app/services/tasks-list.service';
 import { CommonListViewComponent } from '../common-list-view/common-list-view.component';
 
 @Component({
@@ -8,9 +12,18 @@ import { CommonListViewComponent } from '../common-list-view/common-list-view.co
   styleUrls: ['./project-tasks.component.scss']
 })
 export class ProjectTasksComponent extends CommonListViewComponent<Task> implements OnInit {
+  private _projectId: number;
 
-  constructor() {
+  @Input('projectId')
+  set projectId(value: number) {
+    this._projectId = value;
+  }
+
+  constructor(http: HttpClient) {
     super();
+    this._restService = new TasksListService(http);
+    const projectFilter = { name: 'projectId', value: [`${this._projectId}`] } as Filter;
+    this._query = { searchString: '', filters: [projectFilter] } as Query;
   }
 
   ngOnInit(): void {
