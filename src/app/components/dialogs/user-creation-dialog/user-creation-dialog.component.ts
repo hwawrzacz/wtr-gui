11aec4ae8bc308d41d7e812fcf9cb9b377e8ee0b
Hubert <hwawrzacz.dev@gmail.com>
2020-12-14 15:22:02 +0100

    Add basic user creation form

--- src/app/components/dialogs/user-creation-dialog/user-creation-dialog.component.ts
+++ src/app/components/dialogs/user-creation-dialog/user-creation-dialog.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { phoneNumberValidator } from 'src/app/helpers/custom-validators';
+import { PositionStringifier } from 'src/app/helpers/parsers';
+import { Position } from 'src/app/model/enums/position';
 
 @Component({
   selector: 'app-user-creation-dialog',
@@ -6,10 +13,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-creation-dialog.component.scss']
 })
 export class UserCreationDialogComponent implements OnInit {
+  private _form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  //#region Getters and setters
+  get form(): FormGroup {
+    return this._form;
   }
 
+  public get positionsList(): Position[] {
+    return PositionStringifier.positionList;
+  }
+  //#endregion
+
+  constructor(
+    private _dialogRef: MatDialogRef<UserCreationDialogComponent>,
+    private _formBuilder: FormBuilder,
+  ) {
+    this._form = this.buildForm();
+  }
+
+  ngOnInit(): void { }
+
+  /** Common abstract */
+  private buildForm(): FormGroup {
+    return this._formBuilder.group({
+      login: ['', [Validators.required]],
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      phoneNumber: ['', [Validators.required, phoneNumberValidator()]],
+      email: ['', [Validators.required, Validators.email]],
+      role: [null, [Validators.required]],
+    })
+  }
+
+  /** Common */
+  public onSave(): void {
+    // TODO: Call user create method 
+    of(true).pipe(
+      tap(res => {
+        if (res) this._dialogRef.close(true);
+        else this.handleUserNotAdded();
+      })
+    ).subscribe();
+  }
+
+  /** Common */
+  private handleUserNotAdded(): void {
+    console.error('User not added');
+  }
+
+  /** Common */
+  public closeDialog(): void {
+    this._dialogRef.close(null);
+  }
+
+  //#region Helpers
+  /** Common */
+  public hasError(controlName: string): boolean {
+    return this._form.get(controlName).valid;
+  };
+
+  public getErrorMessage(controlName: string): string {
+    const control = this._form.get(controlName);
+    if (control.hasError('required')) return 'Value is required';
+    if (control.hasError('email')) return 'Value must be a valid email';
+    if (control.hasError('phoneNumber')) return 'Value must contain only digits';
+    if (control.hasError('minLength')) return 'Value is too short';
+    if (control.hasError('maxLength')) return 'Value is too long';
+  }
+
+  public getPositionString(position: Position): string {
+    return PositionStringifier.getPositionString(position);
+  }
+  //#endregion
 }
--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { tap } from 'rxjs/operators';
 import { CommonListViewComponent } from 'src/app/components/common-list-view/common-list-view.component';
+import { UserCreationDialogComponent } from 'src/app/components/dialogs/user-creation-dialog/user-creation-dialog.component';
 import { User } from 'src/app/model/user';
 import { UsersRestService } from 'src/app/services/users-rest.service';
 
@@ -10,12 +13,18 @@ import { UsersRestService } from 'src/app/services/users-rest.service';
   styleUrls: ['../../components/common-list-view/common-list-view.component.scss']
 })
 export class UsersListComponent extends CommonListViewComponent<User> {
-  constructor(http: HttpClient) {
+  constructor(http: HttpClient, private _dialogService: MatDialog) {
     super();
 
     this._pageTitle = 'Users'
     this._themeItemNameSingle = 'user';
     this._restService = new UsersRestService(http);
+    this.openUserCreationDialog();
+  }
+
+  /** Common abstract */
+  public openUserCreationDialog(): void {
+    this._dialogService.open(UserCreationDialogComponent)
   }
 
 }
