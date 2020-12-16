43e5167bc15a74dc0241a9db99ecaf6b1a08cac9
Hubert <hwawrzacz.dev@gmail.com>
2020-12-16 15:51:16 +0100

    Rename users-rest.service to users-list.service

---
+++ src/app/services/users-list.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { mockUsers } from '../model/mock-data';
+import { User } from '../model/user';
+import { CommonArrayRestService } from './common-array-rest.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersListService extends CommonArrayRestService<User> {
+  constructor(http: HttpClient) {
+    super(http);
+    this._mockData = mockUsers;
+    this.url = 'users'
+  }
+}
