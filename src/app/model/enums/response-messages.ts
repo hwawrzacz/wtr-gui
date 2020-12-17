aee27074343f985546e3e516440d6372a7796303
Hubert <hwawrzacz.dev@gmail.com>
2020-12-17 18:18:46 +0100

    Update creation response messages

--- src/app/model/enums/response-messages.ts
+++ src/app/model/enums/response-messages.ts
@@ -1,4 +1,5 @@
 export enum CreationResponseMessage {
-  LOGIN_IS_TAKEN = "Login is taken",
-  PROJECT_VALIDATION_FAILED = "projects validation failed",
+  INVALID_DUTY_DATE = 'Duty date is invalid',
+  LOGIN_IS_TAKEN = 'Login is taken',
+  PROJECT_VALIDATION_FAILED = 'projects validation failed',
 }
