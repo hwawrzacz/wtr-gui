a63402566597661ebbbb7c5840b8d0f218696e3b
Hubert <hwawrzacz.dev@gmail.com>
2020-12-24 14:06:53 +0100

    Fix undefined taskId
    Items were initialized in constructor, and not all fields were available
    then.
    That't the reason why you should use ngOnInit() for initializing
    data, as long as you don't need to use constructor parameters.

--- src/app/components/work-logs-list/work-logs-list.component.ts
+++ src/app/components/work-logs-list/work-logs-list.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { USER_ID_MOCK } from 'src/app/model/constants';
 import { Filter } from 'src/app/model/filter';
+import { Pagination } from 'src/app/model/pagination';
 import { Query } from 'src/app/model/query';
 import { WorkLog } from 'src/app/model/work-log';
 import { WorkLogsListRestService } from 'src/app/services/rest/work-logs-list-rest.service';
@@ -20,6 +21,9 @@ export class WorkLogsListComponent extends CommonListViewComponent<WorkLog> impl
   set taskId(value: string) {
     this._taskId = value;
   }
+  get taskId(): string {
+    return this._taskId;
+  }
 
   constructor(
     restService: WorkLogsListRestService,
@@ -27,15 +31,15 @@ export class WorkLogsListComponent extends CommonListViewComponent<WorkLog> impl
     dialogService: MatDialog
   ) {
     super(restService, snackBarService, dialogService);
+  }
 
+  ngOnInit(): void {
     // TODO: Get user id from login service when available
     const userId = USER_ID_MOCK;
     const taskFilter = { name: 'idTask', values: [`${this._taskId}`] } as Filter;
     const userFilter = { name: 'idUser', values: [`${userId}`] } as Filter;
     this._query = { searchString: '', filters: [taskFilter, userFilter] };
-  }
 
-  ngOnInit(): void {
     super.ngOnInit();
   }
 
--- src/app/services/rest/work-logs-list-rest.service.ts
+++ src/app/services/rest/work-logs-list-rest.service.ts
@@ -1,9 +1,5 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable, Query } from '@angular/core';
-import { Observable } from 'rxjs';
-import { CommonArrayResponse } from 'src/app/model/common-array-response';
-import { Pagination } from 'src/app/model/pagination';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { mockWorkLogs } from '../../model/mock-data';
 import { WorkLog } from '../../model/work-log';
 import { CommonListRestService } from './common-list-rest.service';
