9dc2fbe636a3ec140553578a1d895c724eeef9c0
Hubert <hwawrzacz.dev@gmail.com>
2020-12-28 12:30:02 +0100

    Add empty qr code login component

---
+++ src/app/components/dialogs/qr-code-login-dialog/qr-code-login-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-code-login-dialog',
+  templateUrl: './qr-code-login-dialog.component.html',
+  styleUrls: ['./qr-code-login-dialog.component.scss']
+})
+export class QrCodeLoginDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { AppCommonModule } from 'src/app/app-common-module';
 import { CommonMaterialModule } from 'src/app/common-material.module';
 import { FaceLoginDialogComponent } from 'src/app/components/dialogs/face-login-dialog/face-login-dialog.component';
+import { QrCodeLoginDialogComponent } from 'src/app/components/dialogs/qr-code-login-dialog/qr-code-login-dialog.component';
 import { ImageCaptureModule } from 'src/app/components/image-capture/image-capture.module';
 
 @NgModule({
@@ -15,7 +14,8 @@ import { ImageCaptureModule } from 'src/app/components/image-capture/image-captu
     CommonModule,
     CommonMaterialModule,
     AppCommonModule,
-    ImageCaptureModule
+    ImageCaptureModule,
+    QrCodeLoginDialogComponent,
   ],
   exports: [
     FaceLoginDialogComponent
