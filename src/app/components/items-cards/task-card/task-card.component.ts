739383d08bc63f9fa041f011e62ddbaa1f2c871f
Hubert <hwawrzacz.dev@gmail.com>
2021-01-02 13:37:53 +0100

    Implement task card and add task cards list

--- src/app/components/items-cards/task-card/task-card.component.ts
+++ src/app/components/items-cards/task-card/task-card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PriorityStringifier, StatusStringifier, stringifyUser } from 'src/app/helpers/parsers';
+import { Task } from 'src/app/model/task';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-task-card',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-card.component.scss']
 })
 export class TaskCardComponent implements OnInit {
+  private _dutyDate: Date;
+  private _task: Task;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input('task')
+  set task(value: Task) {
+    this._task = value;
+    this._dutyDate = new Date(value.dutyDate)
+  }
+  get task(): Task {
+    return this._task;
   }
 
+  get statusString(): string {
+    return StatusStringifier.getStatusString(this._task.status);
+  }
+
+  get priorityString(): string {
+    return PriorityStringifier.getPriorityString(this._task.priority);
+  }
+
+  get dutyDate(): string {
+    return this._dutyDate.toLocaleDateString('pl-PL');
+  }
+  constructor() { }
+
+  ngOnInit(): void { }
 }
