4c0bfe78067086cafa6cde5255a1011b0be691c9
Hubert <hwawrzacz.dev@gmail.com>
2021-01-02 11:48:36 +0100

    Remove statistics from routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
 import { ProjectsListComponent } from './pages/projects-list/projects-list.component';
-import { StatisticsComponent } from './pages/statistics/statistics.component';
 import { TaskDetailsComponent } from './pages/task-details/task-details.component';
 import { TasksListComponent } from './pages/tasks-list/tasks-list.component';
 import { UsersListComponent } from './pages/users-list/users-list.component';
 import { UserDetailsComponent } from './pages/user-details/user-details.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
-import { StatisticsGuard } from './auth/statistics.guard';
 import { UsersGuard } from './auth/users.guard';
 
 const routes: Routes = [
@@ -21,7 +19,6 @@ const routes: Routes = [
   { path: 'tasks/:id', component: TaskDetailsComponent, canActivate: [AuthGuard] },
   { path: 'users', component: UsersListComponent, canActivate: [AuthGuard, UsersGuard] },
   { path: 'users/:id', component: UserDetailsComponent, canActivate: [AuthGuard] },
-  { path: 'statistics', component: StatisticsComponent, canActivate: [AuthGuard, StatisticsGuard] },
   { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
